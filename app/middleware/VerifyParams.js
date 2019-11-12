b0a2a579da277e7a0ef6539065e1744c3c8ee245
Thomas Kensah <57236769+the-coding-tom@users.noreply.github.com>
2019-11-12 15:42:26 +0000

    Add files via upload

---
+++ app/middleware/VerifyParams.js
@@ -0,0 +1,13 @@
+﻿const verifyParameters = (req, res, next) => {
+    try {
+        if (!req.body.domain || !req.body.regexes || !req.body.apiKey) throw "Please include the domain and a list of regular expressions to find in the pages and your api key.";
+
+        if (!req.body.numLevels) req.body.numLevels = 3; // default to 3 levels
+
+        next();
+    } catch (e) {
+        res.status(400).send({ status: "unsuccessful", errorMessage: e });
+    }
+};
+
+export default verifyParameters;
