6eda499b81107dbf5d77a4aeec8c0c86cea742b2
Thomas Kensah <tom.kensah@gmail.com>
2020-07-29 12:41:16 +0000

    init

--- app.js
+++ app.js
@@ -2,21 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './app/routes/routes';
 
-//
 const App = express();
 
 // Middleware Filters
-App.use(bodyParser.urlencoded({ extended: true }));
+App.use(bodyParser.urlencoded({
+	extended: true
+}));
 App.use(bodyParser.json());
 
-// catch 400
+// catch HTTP Error 400
 App.use((err, req, res, next) => {
 	console.log(err.stack);
 	res.status(400).send(`Error: ${res.originUrl} not found`);
 	next();
 });
 
-// catch 500
+// catch HTTP Error 500
 App.use((err, req, res, next) => {
 	console.log(err.stack);
 	res.status(500).send(`Error: ${err}`);
